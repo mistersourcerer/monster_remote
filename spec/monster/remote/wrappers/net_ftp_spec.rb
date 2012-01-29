e2e9e86d49248c5912d06638c5f2bea7501bc546
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-01-29 13:36:42 -0200

    preparing specs to net_ftp wrapper

---
+++ spec/monster/remote/wrappers/net_ftp_spec.rb
@@ -0,0 +1,10 @@
+module Monster
+  module Remote
+    module Wrappers
+
+      describe NetFTP, "copy some local directory, to the remote host" do
+      end
+
+    end# Wrappers
+  end# Remote
+end# Monster
