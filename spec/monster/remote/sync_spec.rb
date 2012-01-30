278a5b69f77f25c78647dd6fdd13d5500fd26e2c
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-01-30 11:50:40 -0200

    add some sync specification

--- spec/monster/remote/sync_spec.rb
+++ spec/monster/remote/sync_spec.rb
@@ -8,21 +8,28 @@ module Monster
 
       context "#start" do
 
-        it "raise error if asked to start without protocol provider" do
-          error = Monster::Remote::MissingProtocolWrapperError
-          lambda { Sync.new(nil).start }.should raise_error(error)
+        it "raise error if asked to start without protocol wrapper" do
+          missing_wrapper = Monster::Remote::MissingProtocolWrapperError
+          lambda { Sync.new(nil).start }.should raise_error(missing_wrapper)
         end
 
-        # abre conexao
         it "call wrapper's #open" do
           wrapper.should_receive(:open)
           sync.start
         end
 
-        # se der error exception
-        # se der certo
-        # espelha estrutura de dirs local e manda os arquivos
-        # # recursivamente até terminar
+        it "call wrapper's #open" do
+          wrapper.should_receive(:open).and_raise(StandardError)
+          no_connection = Monster::Remote::NoConnectionError
+          lambda{ sync.start }.should raise_error(no_connection)
+        end
+
+        it "call wrapper's #open" do
+          wrapper.should_receive(:copy_dir).with(local_dir_path)
+          # add times here, should receive copy_dir for each dir in the
+          # stucture
+          sync.start
+        end
       end# #start
     end# Sync
   end
