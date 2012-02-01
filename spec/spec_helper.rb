a7ea4c012ecd2dfd7c526137223d62c71b57a4e6
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-02-01 12:01:01 -0200

    less duplication on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,33 @@ end
 def spec_tmp
   File.join(spec_dir, "_tmp")
 end
+
+def local_dir
+  File.join(spec_tmp, "_ftp_")
+end
+
+def remote_dir
+  File.join("tmp", "_ftp_")
+end
+
+def dir_structure
+  {
+    "site" => ["xpto.txt"],
+    "site/images" => ["img1", "img2", "img3"],
+    "site/borba" => ["file1", "file2"],
+    "site/borba/subdir" => ["entaro", "adum", "toredas"],
+    "site/borba/subdir/test" => ["go1", "go2", "go3"]
+  }
+end
+
+def create_dir_structure
+  dir_structure.each do |dir, files|
+    dir = File.join(local_dir, dir)
+    FileUtils.mkdir_p(dir)
+    files.each do |file|
+      File.open(File.join(dir, file), "w") do |f|
+        f.write(file)
+      end
+    end
+  end
+end
